3d6b9e45faa7c9853c9ac1e11c903d9e2a5df7bb
Victor Soares Vilela <dev.victorvilela@gmail.com>
2024-08-15 23:07:32 +0100

    User Registration and activation code created for the User to validate

---
+++ book-network/src/main/java/com/dev_vilela/book/email/EmailTemplateName.java
@@ -0,0 +1,13 @@
+package com.dev_vilela.book.email;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum EmailTemplateName {
+
+    ACTIVATE_ACCOUNT("activate_account");
+
+    private final String name;
+}
