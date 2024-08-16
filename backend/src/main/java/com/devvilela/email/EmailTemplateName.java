3f315021d5d7fb901d8f1b8513e02dfd9beb3cd5
Victor Soares Vilela <dev.victorvilela@gmail.com>
2024-08-16 23:50:36 +0100

    change package name

---
+++ backend/src/main/java/com/devvilela/email/EmailTemplateName.java
@@ -0,0 +1,13 @@
+package com.devvilela.email;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum EmailTemplateName {
+
+    ACTIVATE_ACCOUNT("activate_account");
+
+    private final String name;
+}
