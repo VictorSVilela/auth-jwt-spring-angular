34c1e3e42f6bcb5f114b1e89ca7691761450788c
Victor Soares Vilela <dev.victorvilela@gmail.com>
2024-08-17 20:36:48 +0100

    configured token service

--- frontend/src/app/pages/login/login.component.ts
+++ frontend/src/app/pages/login/login.component.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthenticationService } from '../../services/authentication-service/authentication.service';
+import { TokenService } from '../../services/token/token.service';
 
 @Component({
   selector: 'app-login',
@@ -16,15 +17,20 @@ export class LoginComponent {
   authRequest: AuthenticationRequest = {email: '', password: ''};
   errorMsg: Array<string> = []
 
-  constructor(private router: Router, private authService: AuthenticationService) {}
+  constructor(
+    private router: Router, 
+    private authService: AuthenticationService,
+    private tokenService: TokenService
+  ) {}
   
   login() {
     this.errorMsg = [];
     this.authService.authenticate({
       body: this.authRequest
     }).subscribe({
-      next: () => {
-        this.router.navigate(['teste']);
+      next: (res) => {
+        this.tokenService.token = res.token as string;
+        this.router.navigate(['']);
       },
       error: (err) => {
         console.log(err);
